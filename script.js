3deed62176f3808439a0ae246581cac8808ca16a
Faith-888 <1662168673@qq.com>
2025-04-24 19:17:26 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("微光上线");
+
+const lines = [
+  document.getElementById('line1'),
+  document.getElementById('line2'),
+  document.getElementById('line3'),
+  document.getElementById('line4')
+];
+
+let delay = 1000;
+lines.forEach((line, index) => {
+  setTimeout(() => {
+    line.classList.add('show');
+  }, delay);
+  delay += 2500;
+});
+
+setTimeout(() => {
+  document.getElementById('buttons').classList.add('show');
+}, delay + 2000);
